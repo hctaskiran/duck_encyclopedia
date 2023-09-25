35848fbf1d0e1415ad6598ee5886608cca7342a5
Haktan Can Taşkıran <haktancantaskiran@gmail.com>
2023-09-25 15:54:56 +0300

    update

--- lib/constants/app_bar_constants.dart
+++ lib/constants/app_bar_constants.dart
@@ -3,6 +3,6 @@ import 'colors.dart';
 
 String abTitle = 'Ducky';
 const abStyle = TextStyle(color: whiteColor);
-const abttStyle = TextStyle(fontStyle: FontStyle.italic, fontSize: 24, fontWeight: FontWeight.w800);
+const abttStyle = TextStyle(fontStyle: FontStyle.italic, fontSize: 24, fontWeight: FontWeight.bold);
 const double abElevation = 9;
 final abShape = RoundedRectangleBorder(borderRadius: BorderRadius.circular(30));
